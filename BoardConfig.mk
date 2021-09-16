34471b096640ec6aa3292fe9ae408d4e3bf93463
AdarshGrewal <adarshgrewal@gmail.com>
2021-09-16 17:38:44 +0530

    sweet_k6a: hidl: Include lineage FCM from vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ TARGET_SCREEN_DENSITY := 440
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/lineage_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 
 # Kernel
