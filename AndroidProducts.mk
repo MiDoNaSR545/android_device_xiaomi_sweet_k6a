2a0c199e96b03f5c4985b6f053780ca73efcda08
MiDoNaSR545 <ahmednasr1973913@gmail.com>
2023-11-23 11:02:01 +0000

    sweet_k6a: Initial device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_sweet_k6a.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_sweet_k6a-user \
+    lineage_sweet_k6a-userdebug \
+    lineage_sweet_k6a-eng
